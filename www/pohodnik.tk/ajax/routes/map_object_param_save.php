efe1bd053e066598f3093ecb0ec3921d0c079535
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-03-31 20:37:29 +0300

    Update map_object_param_save.php

--- www/pohodnik.tk/ajax/routes/map_object_param_save.php
+++ www/pohodnik.tk/ajax/routes/map_object_param_save.php
@@ -9,7 +9,7 @@ $id_user = isset($_COOKIE["user"])?$_COOKIE["user"]:0;
 if($id>0 && $id_user>0){
 
 	$q = $mysqli->query("UPDATE route_objects 
-						SET `{$name}`='{$value}', id_editor={$id_user}, date_last_modif='".date('d.m.Y H:i:s')."' 
+						SET `{$name}`='{$value}', id_editor={$id_user}, date_last_modif='".date('Y-m-d H:i:s')."' 
 						WHERE id={$id}
 						");
 	if($q){
