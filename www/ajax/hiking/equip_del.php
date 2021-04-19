54d02724526b202544841c7829eeafc735f2a975
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-04-19 23:01:12 +0300

    HikingEquipAdmin

--- www/ajax/hiking/equip_del.php
+++ www/ajax/hiking/equip_del.php
@@ -2,6 +2,18 @@
 include("../../blocks/db.php"); //подключение к БД
 include("../../blocks/for_auth.php"); //Только для авторизованных
 $id = isset($_POST['id'])?intval($_POST['id']):0;
+
+$id_hiking = intval($_POST['id_hiking']);
+$id_user = $_COOKIE["user"];
+
+$q = $mysqli->query("SELECT id FROM hiking WHERE id_author={$id_user} AND id={$id_hiking}");
+if(!$q || $q->num_rows===0){
+	$q = $mysqli->query("SELECT id FROM hiking_editors WHERE id_user={$id_user} AND id_hiking={$id_hiking}");
+	if(!$q || $q->num_rows===0){
+		die(json_encode(array("error"=>"Нет доступа . \r\n".$mysqli->error)));
+	}
+}
+
 if($id>0){
 	if($mysqli->query("DELETE FROM `hiking_equipment` WHERE id={$id}  ")){
 		exit(json_encode(array("success"=>"Запись удалена", "id"=> $id)));
--- www/ajax/hiking/equip_save.php
+++ www/ajax/hiking/equip_save.php
@@ -1,23 +1,46 @@
 <?php
 include("../../blocks/db.php"); //подключение к БД
 include("../../blocks/for_auth.php"); //Только для авторизованных
-$result = array();
 
 $id_hiking = intval($_POST['id_hiking']);
-$id_equip = intval($_POST['id_equip']);
 $id_user = $_COOKIE["user"];
 
-$q = $mysqli->query("SELECT id FROM hiking_equipment WHERE `id_hiking`={$id_hiking} AND `id_user`={$id_user} AND `id_equip`={$id_equip} LIMIT 1");
-if($q && $q->num_rows===1){
-	exit(json_encode(array("error"=>"Уже добавлено")));
+$q = $mysqli->query("SELECT id FROM hiking WHERE id_author={$id_user} AND id={$id_hiking}");
+if(!$q || $q->num_rows===0){
+	$q = $mysqli->query("SELECT id FROM hiking_editors WHERE id_user={$id_user} AND id_hiking={$id_hiking}");
+	if(!$q || $q->num_rows===0){
+		die(json_encode(array("error"=>"Нет доступа . \r\n".$mysqli->error)));
+	}
 }
-	if($mysqli->query("INSERT INTO `hiking_equipment` SET
-						`id_hiking`={$id_hiking},
-						`id_user`={$id_user},
-						`id_equip`={$id_equip}
-					  ")){
-		$id = $mysqli->insert_id;
-		exit(json_encode(array("success"=>"Добавлено: ".$name."", "id"=> $id)));
-	}else{exit(json_encode(array("error"=>"Ошибка добавления. \r\n".$mysqli->error)));}
 
-?>
+$id = intval($_POST['id']);
+$name = $mysqli->real_escape_string(trim($_POST['name']));
+$user = isset($_POST['id_user']) && $_POST['id_user'] > 0 ? intval($_POST['id_user']) : 'NULL';
+$weight = floatval($_POST['weight']);
+$value = floatval($_POST['value']);
+$is_confirm = boolval($_POST['is_confirm']);
+$photo = $mysqli->real_escape_string(trim($_POST['photo']));
+
+$action = $id > 0 ? 'UPDATE' : 'INSERT INTO';
+$claus = $id > 0 ? 'WHERE id='.$id : '';
+
+$z = "
+	{$action}
+		`hiking_equipment`
+	SET
+		`id_hiking`={$id_hiking},
+		`id_user`={$user},
+		`name`='{$name}',
+		`weight`={$weight},
+		`value`={$value},
+		`is_confirm`={$is_confirm},
+		`photo`='{$photo}',
+		`id_author`={$id_user},
+		`date_create`=NOW()
+	{$claus}
+";
+
+$q = $mysqli->query($z);
+if (!$q) { exit(json_encode(array("error"=>"Ошибка {$action}. \r\n".$mysqli->error))); }
+$id = $mysqli->insert_id;
+exit(json_encode(array("success"=>true, "id"=> $id)));
