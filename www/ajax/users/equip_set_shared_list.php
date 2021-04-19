0ab345d9ddb1002b0041934943fa816383eec346
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-04-19 20:33:50 +0300

    shared equip list

---
+++ www/ajax/users/equip_set_shared_list.php
@@ -0,0 +1,34 @@
+<?
+include("../../blocks/db.php"); //подключение к БД
+include("../../blocks/for_auth.php"); //Только для авторизованных
+
+if (!isset($_GET['id_hiking'])) { die(json_encode(array("error"=>"id_hiking is required"))); }
+$id_hiking = intval($_GET['id_hiking']);
+
+$id_current_user = $_COOKIE["user"];
+
+$z = "
+SELECT
+    sh.*,
+    users.name,
+    users.surname,
+    users.photo_50 as photo,
+    own.name as own_name,
+    own.surname as own_surname,
+    own.photo_50 as own_photo
+FROM
+    `user_equip_sets_share` as sh
+    LEFT JOIN users ON users.id = sh.to_user
+    LEFT JOIN user_equip_sets ON user_equip_sets.id = sh.id_set
+    LEFT JOIN users AS own ON user_equip_sets.id_user = own.id
+WHERE
+    id_hiking={$id_hiking}
+";
+
+$q = $mysqli->query($z);
+if(!$q){die(json_encode(array("error"=>$mysqli->error)));}
+$res = array();
+while($r = $q -> fetch_assoc()) {
+    $res[] = $r;
+}
+die(json_encode($res));
