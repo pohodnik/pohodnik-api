32b6164de6a8f07b8debfaee8189d5448b135e64
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-04-29 00:05:10 +0300

    fix mysql conf

--- www/ajax/hiking/menu/products.php
+++ www/ajax/hiking/menu/products.php
@@ -9,45 +9,63 @@ $addwhere = "";
 if(isset($_GET['id_act'])){$addwhere .= " AND hiking_menu.id_act=".intval($_GET['id_act'])." ";}
 if(isset($_GET['date'])){$addwhere .= " AND hiking_menu.date='".$mysqli->real_escape_string($_GET['date'])."' ";}
 
-$q = $mysqli->query("
-SELECT
+$q = $mysqli->query("SELECT
 recipes_products.name,
-SUM(recipes_structure.amount*(hiking_menu.сorrection_coeff_pct/100)) AS amount,	
+SUM(recipes_structure.amount * (hiking_menu.сorrection_coeff_pct / 100)) AS amount,	
 recipes_products.id AS id_product,
+0 AS is_optimize,
+hiking_menu_products_force.id AS id_force,
+CONCAT(forceUser.name,' ',forceUser.surname) AS forceUserName,
 
 GROUP_CONCAT(
-	CONCAT(
+	CONCAT_WS(
+		'|',
 		recipes.name,
-		'|',
 		hiking_menu.date,
-		'|',
-		recipes_structure.amount*(hiking_menu.сorrection_coeff_pct/100)
+		recipes_structure.amount * (hiking_menu.сorrection_coeff_pct / 100)
 	)
 ) AS use9,
 
-0 AS is_optimize,
-(SELECT GROUP_CONCAT(CONCAT(weight,'|',value,'|', cost)) 
-FROM recipes_products_units_values WHERE id_product=recipes_products.id GROUP BY recipes_products_units_values.id_product) AS cost,
-hiking_menu_products_force.id AS id_force,
-CONCAT(forceUser.name,' ',forceUser.surname) AS forceUserName
+(
+	SELECT
+		GROUP_CONCAT(
+			CONCAT_WS('|', weight, value, cost)
+		) as g
+	FROM
+		recipes_products_units_values
+	WHERE
+		id_product = recipes_products.id
+		GROUP BY id_product
+) AS cost,
+(
+	SELECT
+		GROUP_CONCAT(
+			CONCAT_WS('|', weight, weight / 1000, cost)
+		) as g
+	FROM
+		hiking_finance
+	WHERE
+		id_product = recipes_products.id
+	GROUP BY id_product
+) AS cost1
 
 FROM hiking_menu
-LEFT JOIN recipes ON recipes.id = hiking_menu.id_recipe 
-LEFT JOIN recipes_structure ON recipes_structure.id_recipe = recipes.id
-LEFT JOIN recipes_products ON  recipes_structure.id_product = recipes_products.id
-LEFT JOIN hiking_menu_products_force ON (
-	hiking_menu_products_force.id_product = recipes_products.id
-	AND hiking_menu_products_force.id_hiking={$id_hiking})
-LEFT JOIN users AS forceUser ON  hiking_menu_products_force.id_user = forceUser.id
-
+	LEFT JOIN recipes ON recipes.id = hiking_menu.id_recipe 
+	LEFT JOIN recipes_structure ON recipes_structure.id_recipe = recipes.id
+	LEFT JOIN recipes_products ON  recipes_structure.id_product = recipes_products.id
+	LEFT JOIN hiking_menu_products_force ON (
+		hiking_menu_products_force.id_product = recipes_products.id
+		AND hiking_menu_products_force.id_hiking={$id_hiking})
+	LEFT JOIN users AS forceUser ON  hiking_menu_products_force.id_user = forceUser.id
 
 WHERE hiking_menu.id_hiking={$id_hiking} ".$addwhere." GROUP BY id_product");//
 if(!$q){die(json_encode(array("error"=>$mysqli->error)));}
 while($r = $q->fetch_assoc()){
 	$res[] = $r;
 }
+
 if(!isset($_GET['view'])){
-exit(json_encode($res));
+	exit(json_encode($res));
 } else {
 	echo '<html><head><meta charset="utf-8"></head><body><table border="1" cellspacing=0 cellpadding=3>';
 	echo '<tr>';
