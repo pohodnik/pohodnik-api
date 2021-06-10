9ef49a6db6d06c91a4a4196441bcc49ed9c55dc2
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-06-10 19:32:05 +0300

    image uploader

--- www/ajax/saveBase64img.php
+++ www/ajax/saveBase64img.php
@@ -1,14 +1,8 @@
 <?php
+	ini_set('memory_limit', '256M');
 	include("../blocks/for_auth.php"); //Только для авторизованных
-	ini_set('error_reporting', E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	function ErrorCatcher($errno, $errstr, $errfile, $errline){
-		die(json_encode(array("error"=>$errstr."\n(".$errfile.":".$errline.")", "errno"=>$errno, "file"=>$errfile, "line"=>$errline )));
-		exit();
-		return false;
-	}
-	set_error_handler("ErrorCatcher");
+	include("../blocks/err.php"); //Только для авторизованных
+
 	$imageData = $_POST['data'];
 	$name = isset($_POST['name'])?$_POST['name']:'file_'.md5(time());
 	$folder = trim(isset($_POST['folder'])?$_POST['folder']:'images/custom/', "/");
@@ -19,31 +13,53 @@
 	$fileName = $name.'.'.$extension;
 	$imageData = base64_decode($imageData);
 	if(file_exists ('../'.$folder."/".$fileName )){ $fileName = '_'.$name.'.'.$extension; }
+
+	if(!file_exists ('../'.$folder."/" )){
+		if(!mkdir('../'.$folder."/", 0777, true)) {
+			die(json_encode(array("error"=>"Не удалось создать папку ".'../'.$folder."/")));	
+		}
+	}
+
 	file_put_contents('../'.$folder."/".$fileName, $imageData);
 	if(file_exists ('../'.$folder."/".$fileName )){ 
 	$resSizes= array();
 
 		if(isset($_POST['sizes'])){
-			if(is_array($_POST['sizes'])){$sizes = $_POST['sizes'];} else { $sizes = explode(",", $_POST['sizes']);}
-			if(count($sizes)>0){
-				list($width, $height) = getimagesize('../'.$folder."/".$fileName);
-				$coeff = $width / $height;
+			if(is_array($_POST['sizes'])){
+				$sizes = $_POST['sizes'];
+			} else {
+				$sizes = explode(",", $_POST['sizes']);
+			}
+			if(count($sizes) > 0){
+				list($originalWidth, $originalHeight) = getimagesize('../'.$folder."/".$fileName);
+				$coeff = $originalWidth / $originalHeight;
+				$width = $originalWidth;
+				$height = $originalHeight;
+
 				require_once('lib/php-image-magician/php_image_magician.php');
-				foreach ( $sizes as $size) {
-					if(strpos($size,"/")>0){
-						$tmp = explode("/",$size);
+				$lastSuccessSize = $originalWidth;
+
+				foreach ($sizes as $size) {
+					
+					if(strpos($size, "/") > 0){
+						$tmp = explode("/", $size);
 						$width = intval($tmp[0]);
 						$height = intval($tmp[1]);
-
 					} else {
 						$width = intval($size);
 						$height = intval($size) / $coeff;
 					}
-					$magicianObj = new imageLib('../'.$folder."/".$fileName);
-					$magicianObj -> resizeImage($width, $height, 'crop');
-					$resFile = $folder."/".$name.'_'.$width."_".$height.'.'.$extension;
-					$magicianObj -> saveImage('../'.$resFile);
-					$resSizes[$size] = $resFile;
+
+					if ($width <= $originalWidth && $height <= $originalHeight) {		
+						$magicianObj = new imageLib('../'.$folder."/".$fileName);
+						$magicianObj -> resizeImage($width, $height, 'crop');
+						$resFile = $folder."/".$name.'_'.$width."_".$height.'.'.$extension;
+						$magicianObj -> saveImage('../'.$resFile);
+						$resSizes[$size] = $resFile;
+						$lastSuccessSize = $size;
+					} else {
+						$resSizes[$size] = $resSizes[$lastSuccessSize];	
+					}
 
 				}
 			}
