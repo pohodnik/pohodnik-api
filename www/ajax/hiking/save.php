5556390cda01c3a484e67d5b0a21b67f017f66f3
Pohodnik EnvyBook <pohodnik58@gmail.com>
2021-10-21 23:21:14 +0300

    Update save.php

--- www/ajax/hiking/save.php
+++ www/ajax/hiking/save.php
@@ -12,7 +12,7 @@ $id = isset($_POST['id'])?intval($_POST['id']):0;
 $id_route = intval($_POST['id_route']);
 $id_region = intval($_POST['id_region']);
 $color = ($mysqli->real_escape_string(trim($_POST['color'])));
-$is_vacant_route = intval($mysqli->real_escape_string(trim($_POST['is_vacant_route']))) > 0 ? 1 : 0;
+$is_vacant_route = $_POST['is_vacant_route'] == 'true' ? 1 : 0;
 
 if($d1>=$d2){exit(json_encode(array("error"=>"Дата начала должна быть раньше даты завершения")));}
 
