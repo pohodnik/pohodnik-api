c0989d78d5d0bd64583c6a5b13af69343a8cf175
Николай Федосеев <xneek@mail.ru>
2022-07-01 15:46:16 +0300

    Update providers_list.php

--- www/ajax/auth/providers_list.php
+++ www/ajax/auth/providers_list.php
@@ -1,8 +1,12 @@
 <?php
-    require_once '../lib/SocialAuther/autoload.php';
-    include './adapters_config.php';
-    include '../../blocks/global.php';
+    ini_set('error_reporting', E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    require_once __DIR__.'../lib/SocialAuther/autoload.php';
+    include __DIR__.'./adapters_config.php';
+    include __DIR__.'../../blocks/global.php';
     $adapters = array();
+
 
     $res = array();
     foreach ($adapterConfigs as $adapter => $settings) {
