3fefa4be7aeb92fcec1fa6300f104b17130858f5
Николай Федосеев <xneek@mail.ru>
2022-12-28 22:10:00 +0300

    Update year_stat_email.php

--- www/cron/year_stat_email.php
+++ www/cron/year_stat_email.php
@@ -53,7 +53,7 @@ while ($r = $q -> fetch_assoc()) {
     </html>"; 
     $to = $r['addresses'];
    // $headers = "From: Почтовый сервис сайта Походники <info@pohodnik.tk>\r\n";
-    $headers .= "Reply-To: info@pohodnik.tk\r\n";
+    $headers = "Reply-To: info@pohodnik.tk\r\n";
     $headers .= "X-Mailer: pohodnik ".phpversion()."\r\n";
     $headers  .= "Content-type: text/html; charset=utf-8\r\n"; 
     
