ae37a9f10e2a53da93a31879ecf5d84b5ab9c22f
Николай Федосеев <xneek@mail.ru>
2023-04-12 20:52:31 +0300

    map save param (no image case)

--- www/ajax/routes/map_save_param.php
+++ www/ajax/routes/map_save_param.php
@@ -1,8 +1,7 @@
 <?php
 include("../../blocks/db.php"); //подключение к БД
 include("../../blocks/for_auth.php"); //Только для авторизованных
-include("../../blocks/imagesStorage.php"); //Только для авторизованных
-include(__DIR__."/../../vendor/autoload.php"); //Только для авторизованных
+
 $result = array();
 $id = intval($_POST['id']);
 $name = $mysqli->real_escape_string($_POST['name']);
@@ -10,6 +9,8 @@ $value = $mysqli->real_escape_string($_POST['value']);
 $id_user = $_COOKIE["user"];
 
 if ($name == 'preview_img' && $id > 0) {
+    include("../../blocks/imagesStorage.php"); //Только для авторизованных
+    include(__DIR__."/../../vendor/autoload.php"); //Только для авторизованных
     $z = "SELECT preview_img FROM routes WHERE id={$id} LIMIT 1";
     $q = $mysqli->query($z);
     if (!$q) {
