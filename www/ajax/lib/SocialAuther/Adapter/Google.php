1462f1ea7f5083d49dac0e77a47538bc9ab82f08
Николай Федосеев <xneek@mail.ru>
2024-01-20 18:21:51 +0300

    google offline

--- www/ajax/lib/SocialAuther/Adapter/Google.php
+++ www/ajax/lib/SocialAuther/Adapter/Google.php
@@ -113,7 +113,7 @@ class Google extends AbstractAdapter
                 'redirect_uri'  => $this->redirectUri,
                 'response_type' => 'code',
                 'client_id'     => $this->clientId,
-                'scope'         => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+                'scope'         => 'https://www.googleapis.com/auth/userinfo.profile'
             )
         );
     }
