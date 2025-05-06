a0b0c3c767c8671e2957b4a91d9d8ee8644b8b79
Николай Федосеев <xneek@mail.ru>
2025-05-06 10:50:00 +0300

    Update Telegram.php

--- www/ajax/lib/SocialAuther/Adapter/Telegram.php
+++ www/ajax/lib/SocialAuther/Adapter/Telegram.php
@@ -79,9 +79,14 @@ class Telegram extends AbstractAdapter
         sort($dataCheckArr);
         $dataCheckString = implode("\n", $dataCheckArr);
         
-        $secretKey = hash_hmac('sha256', $this->clientSecret,"WebAppData",true);
+        $secretKey = hash('sha256', $this->clientSecret, true);
         $hash = hash_hmac('sha256', $dataCheckString, $secretKey);
-        return strcmp($hash, $checkHash) === 0;
+
+        if (strcmp($hash, $checkHash) === 0) {
+            return true;
+        }
+
+        return array('need' => $secretKey, 'has' => $hash);
     }
 
     /**
