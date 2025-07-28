d1fb4edf48265d578b789cdf1c1adcc93e04d7b6
Походник Macbook <pohodnik58@gmail.com>
2025-07-28 23:32:54 +0300

    Update recipes_get_all.php

--- www/ajax/recipes/recipes_get_all.php
+++ www/ajax/recipes/recipes_get_all.php
@@ -44,18 +44,20 @@ if($for_hiking>0){
 	$claus .= " AND hiking_recipes.id_hiking={$for_hiking}";
 } 
 
-	$q2 = $mysqli->query("	SELECT 
-								recipes.id, recipes.name, recipes.photo, recipes.promo_text, recipes.id_author, (recipes.id_author=".$id_user.") AS my ,
-								SUM((recipes_products.protein/100)*recipes_structure.amount) AS protein,
-								SUM((recipes_products.fat/100)*recipes_structure.amount) AS fat,
-								SUM((recipes_products.carbohydrates/100)*recipes_structure.amount) AS carbohydrates,
-								SUM((recipes_products.energy/100)*recipes_structure.amount) AS energy,
-								SUM(recipes_structure.amount) as amount
-							FROM recipes 
-								LEFT JOIN recipes_structure ON recipes_structure.id_recipe = recipes.id
-								LEFT JOIN recipes_products ON  recipes_structure.id_product = recipes_products.id
-								{$joins}
-							WHERE recipes.id_category=".$r['id']." {$claus} GROUP BY recipes.id");
+	$q2 = $mysqli->query("
+    SELECT 
+        recipes.id, recipes.name, recipes.photo, recipes.promo_text, recipes.id_author, (recipes.id_author=".$id_user.") AS my ,
+        SUM((recipes_products.protein/100)*recipes_structure.amount) AS protein,
+        SUM((recipes_products.fat/100)*recipes_structure.amount) AS fat,
+        SUM((recipes_products.carbohydrates/100)*recipes_structure.amount) AS carbohydrates,
+        SUM((recipes_products.energy/100)*recipes_structure.amount) AS energy,
+        SUM(recipes_structure.amount) as amount
+    FROM recipes 
+        LEFT JOIN recipes_structure ON recipes_structure.id_recipe = recipes.id
+        LEFT JOIN recipes_products ON  recipes_structure.id_product = recipes_products.id
+        {$joins}
+    WHERE recipes.id_category=".$r['id']." {$claus} GROUP BY recipes.id
+    ");
 	if(!$q2){die($mysqli->error);}
 	while($r2 = $q2->fetch_assoc()){
 		$r["items"][] = $r2;
